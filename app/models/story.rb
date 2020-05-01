17b89de1fb209250913eae9a800820c4c277206c
Xinya Yang <xinyayang0506@gmail.com>
2020-05-01 11:20:42 -0500

    change validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,14 +1,14 @@
 class Story < ApplicationRecord
     validates :PPE_type1, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
     validates :Amount1, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
     validates :Recipient1, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
     validates :PPE_type2, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
     validates :Amount2, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
     validates :Recipient2, presence: true,
-    length: {minimum: 3, maximum: 30}
+    length: {minimum: 1, maximum: 30}
 end
